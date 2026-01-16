ea96847024476e84bc1e7ac0cfc6c5248357fb30
Juan Gomez Guzman <104452048+juanmgomez7152@users.noreply.github.com>
2026-01-16 11:44:22 -0600

    Added the tech stack skills to the Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Plasma from "./components/Plasma";
+import LogoLoop from "./components/LogoLoop";
+import techLogos from "./assets/tech-stack";
 
 function App() {
   return (
-    <div style={{
-      background:'black',
-      display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-      minHeight:'100vh'
-    }
-    }>
+    <div
+      style={{
+        background: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
       <div style={{ width: "100%", height: "100vh", position: "relative" }}>
         <Plasma
           color="#2F4F6F"
@@ -19,6 +22,31 @@ function App() {
           opacity={0.8}
           mouseInteractive={true}
         />
+        <div
+          style={{ 
+            height: "100px", 
+            position: "absolute", 
+            overflow: "hidden",
+            left:0,
+            right:0,
+            display: 'flex',
+            bottom:'5px',
+            color:'white'
+          }}
+        >
+          <LogoLoop
+            logos={techLogos}
+            speed={120}
+            direction="left"
+            logoHeight={48}
+            gap={40}
+            hoverSpeed={0}
+            scaleOnHover
+            fadeOut
+            fadeOutColor='black'
+            ariaLabel="Technology partners"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/assets/tech-stack.tsx
@@ -0,0 +1,28 @@
+import {
+  SiReact,
+  SiTypescript,
+  SiPython,
+  SiGit,
+  SiDocker,
+  SiOpenai,
+  SiGooglecloud,
+  SiAngular,
+  SiFastapi,
+  SiGithub
+} from "react-icons/si"; // Simple Icons
+
+const techLogos = [
+  { node: <SiReact />, title: "React" },
+  { node: <SiTypescript />, title: "TypeScript" },
+  { node: <SiPython />, title: "Python" },
+  { node: <SiGit />, title: "Git" },
+  { node: <SiGithub/>, title: "Github" },
+  { node: <SiDocker />, title: "Docker" },
+  { node: <SiOpenai />, title: "Openai" },
+  { node: <SiGooglecloud />, title: "Google Cloud Platform" },
+  { node: <SiAngular />, title: "Angular" },
+  { node: <SiFastapi/>, title: "FastApi" },
+];
+
+
+export default techLogos
