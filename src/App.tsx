f798865326e148bcc324ef42dee55c645c486e03
Juan Gomez Guzman <104452048+juanmgomez7152@users.noreply.github.com>
2026-01-16 10:51:16 -0600

    Added shadnc(react bits package) and implemented the Plasma background

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Plasma from "./components/Plasma";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{
+      background:'black',
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      minHeight:'100vh'
+    }
+    }>
+      <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+        <Plasma
+          color="#2F4F6F"
+          speed={0.6}
+          direction="forward"
+          scale={1.1}
+          opacity={0.8}
+          mouseInteractive={true}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
